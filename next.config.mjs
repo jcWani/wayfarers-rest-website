849f8fe5a0da0bdd5808d4cb6622d6f62de3cc7d
jcWani <james.waniwan01@gmail.com>
2025-02-10 00:02:44 +0800

    Update supabase

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cjaylxcmzxbovendcgcs.supabase.co",
+        hostname: "ziydlavsgtvotxmrnjpu.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/room-images/**",
       },
